dcc8bd1eb4e077f8b5dd448ce9bfcb1d7e931310
lx <liu.hanyu@pku.edu.cn>
2024-10-17 18:28:14 +0800

    Update demo.py

--- scripts/demo/retriever/demo.py
+++ scripts/demo/retriever/demo.py
@@ -2,7 +2,8 @@ from transformers import AutoModel, AutoTokenizer
 import torch
 import torch.nn.functional as F
 from PIL import Image
-import os
+import requests
+from io import BytesIO
 
 def weighted_mean_pooling(hidden, attention_mask):
     attention_mask_ = attention_mask * attention_mask.cumsum(dim=1)
@@ -26,7 +27,6 @@ def encode(text_or_image_list):
             'image': text_or_image_list,
             'tokenizer': tokenizer
         }
-
     outputs = model(**inputs)
     attention_mask = outputs.attention_mask
     hidden = outputs.last_hidden_state
@@ -35,21 +35,24 @@ def encode(text_or_image_list):
     embeddings = F.normalize(reps, p=2, dim=1).detach().cpu().numpy()
     return embeddings
 
-model_name_or_path = "openbmb/VisRAG"
-tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, trust_remote_code=True)
-model = AutoModel.from_pretrained(model_name_or_path,torch_dtype=torch.bfloat16, trust_remote_code=True).to("cuda")
+tokenizer = AutoTokenizer.from_pretrained("/mnt/data/user/tc_agi/klara/datasets/visrag_ret/visrag_ret", trust_remote_code=True)
+model = AutoModel.from_pretrained("/mnt/data/user/tc_agi/klara/datasets/visrag_ret/visrag_ret", torch_dtype=torch.bfloat16, trust_remote_code=True)
 model.eval()
 
-
-script_dir = os.path.dirname(os.path.realpath(__file__))
 queries = ["What does a dog look like?"]
-passages = [
-    Image.open(os.path.join(script_dir, 'test_image/cat.jpeg')).convert('RGB'),
-    Image.open(os.path.join(script_dir, 'test_image/dog.jpg')).convert('RGB'),
-]
-
 INSTRUCTION = "Represent this query for retrieving relevant documents: "
 queries = [INSTRUCTION + query for query in queries]
+
+print("Downloading images...")
+passages = [
+    Image.open(BytesIO(requests.get(
+        'https://github.com/OpenBMB/VisRAG/raw/refs/heads/master/scripts/demo/retriever/test_image/cat.jpeg'
+    ).content)).convert('RGB'),
+    Image.open(BytesIO(requests.get(
+        'https://github.com/OpenBMB/VisRAG/raw/refs/heads/master/scripts/demo/retriever/test_image/dog.jpg'
+    ).content)).convert('RGB')
+]
+print("Images downloaded.")
 
 embeddings_query = encode(queries)
 embeddings_doc = encode(passages)
